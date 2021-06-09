2609c668f9c0e4b2a30e5e573b1ea8645462872e
Karen Lorhana <43828995+karenlorhana@users.noreply.github.com>
2021-06-09 10:06:24 -0300

    Calculator (Update)

--- calculator/calc.js
+++ calculator/calc.js
@@ -4,25 +4,32 @@
 */
 Object.defineProperty(exports, "__esModule", { value: true });
 const readline_sync_1 = require("readline-sync");
-function main() {
-    const numero1 = readline_sync_1.question('Insira o primeiro numero: \n');
-    const operador = readline_sync_1.question('insira o operador: \n');
-    const numero2 = readline_sync_1.question('Insira o segundo numero: \n');
-    // const numero1: number = parseInt(numero1);
-    // const numero2: number = parseInt(numero2);
+const calcular = (numero1, operador, numero2) => {
+    switch (operador) {
+        case "+":
+            return numero1 + numero2;
+        case "-":
+            return numero1 - numero2;
+        case "*":
+            return numero1 * numero2;
+        case "/":
+            if (numero2 == 0) {
+                throw new Error("0 division");
+            }
+            return numero1 / numero2;
+        default:
+            throw new Error("invalid operation");
+    }
+};
+const numero1 = readline_sync_1.questionInt("Insira o primeiro numero: \n");
+const operador = readline_sync_1.question("insira o operador: \n");
+const numero2 = readline_sync_1.questionInt("Insira o segundo numero: \n");
+// const numero1: number = parseInt(numero1);
+// const numero2: number = parseInt(numero2);
+try {
     const resultado = calcular(numero1, operador, numero2);
     console.log(resultado);
-    function calcular(numero1, operador, numero2) {
-        switch (operador) {
-            case '+':
-                return numero1 + numero2;
-            case '-':
-                return numero1 - numero2;
-            case '*':
-                return numero1 * numero2;
-            case '/':
-                return numero1 / numero2;
-        }
-    }
 }
-main();
+catch (error) {
+    console.error(error.message);
+}
--- calculator/calc.ts
+++ calculator/calc.ts
@@ -1,34 +1,41 @@
-/*  \\\\\\\\
-
+/* 
+npm install --save readline-sync
+npm i -D @types/readline-sync
 */
 
-type Operador = '+' | '-' | '*' | '/';
+import { question, questionInt } from "readline-sync";
 
-import { question } from 'readline-sync';
+type Operador = "+" | "-" | "*" | "/";
 
-function main(): void {
-
-  const numero1: string = question('Insira o primeiro numero: \n');
-  const operador: string = question('insira o operador: \n');
-  const numero2: string = question('Insira o segundo numero: \n');
-
-  // const numero1: number = parseInt(numero1);
-  // const numero2: number = parseInt(numero2);
-  const resultado = calcular(numero1, operador as Operador, numero2)
-  console.log(resultado);
-
-
-  function calcular(numero1: number, operador: Operador, numero2: number) {
-    switch (operador) {
-      case '+':
-        return numero1 + numero2;
-      case '-':
-        return numero1 - numero2;
-      case '*':
-        return numero1 * numero2;
-      case '/':
-        return numero1 / numero2;
-    }
+const calcular = (numero1: number, operador: Operador, numero2: number) => {
+  switch (operador) {
+    case "+":
+      return numero1 + numero2;
+    case "-":
+      return numero1 - numero2;
+    case "*":
+      return numero1 * numero2;
+    case "/":
+      if (numero2 == 0) {
+        throw new Error("0 division");
+      }
+      return numero1 / numero2;
+    default:
+      throw new Error("invalid operation");
   }
 }
-main();
+
+const numero1 = questionInt("Insira o primeiro numero: \n");
+const operador: string = question("insira o operador: \n");
+const numero2 = questionInt("Insira o segundo numero: \n");
+
+try {
+  const resultado = calcular(
+    numero1,
+    operador as Operador,
+    numero2
+  );
+  console.log(resultado);
+} catch (error) {
+  console.error(error.message);
+}
